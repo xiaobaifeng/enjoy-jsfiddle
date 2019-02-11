abd3c96e8a29e609962ba9b7d70adc91e17cde2c
cwb <sy_caowubin@aliyun.com>
2019-02-11 12:03:09 +0800

    feat：初始化

---
+++ docs/precache-manifest.8139e6a77358c80898b05a87c9a10b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4bb857a44170d729b776",
+    "url": "/js/chunk-vendors.fb18aed6.js"
+  },
+  {
+    "revision": "0df32a57df5c1ff2b966",
+    "url": "/js/app.77fcdc26.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "/js/about.0d02e657.js"
+  },
+  {
+    "revision": "02b8fa9dfb2e581d3de13e3d152183be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0df32a57df5c1ff2b966",
+    "url": "/css/app.48f5c24c.css"
+  }
+];
