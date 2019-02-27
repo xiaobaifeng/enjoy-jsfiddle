80279de73fea7c19ffae6338db2ff0b659c71a12
cwb <sy_caowubin@aliyun.com>
2019-02-27 17:22:06 +0800

    docs:run build

---
+++ docs/precache-manifest.8cfb74f4c9ab88cd5380a68bb5baf0d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cee68faae739045345d6",
+    "url": "js/jsfiddle.17321784.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "4bb857a44170d729b776",
+    "url": "js/chunk-vendors.fb18aed6.js"
+  },
+  {
+    "revision": "03b14af830b99aff3e13",
+    "url": "js/chunk-22a54528.1200070c.js"
+  },
+  {
+    "revision": "9b93bb602da38d808a1a",
+    "url": "js/app.cef0c525.js"
+  },
+  {
+    "revision": "f53d31fa37c5bb3296aad974f606ae79",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "35e8fd87c8e8835621888ee611d1158b",
+    "url": "img/XN.35e8fd87.jpg"
+  },
+  {
+    "revision": "eb913ea8946ac45cc0492295dea4a8d4",
+    "url": "img/68747470733a2f2f706963322e7a68696d672e636f6d2f37313337373464353338336437323662316362386536623665663438666430645f622e706e67.eb913ea8.png"
+  },
+  {
+    "revision": "cee68faae739045345d6",
+    "url": "css/jsfiddle.7cb58249.css"
+  },
+  {
+    "revision": "03b14af830b99aff3e13",
+    "url": "css/chunk-22a54528.aa1810b4.css"
+  },
+  {
+    "revision": "9b93bb602da38d808a1a",
+    "url": "css/app.e768497b.css"
+  }
+];
