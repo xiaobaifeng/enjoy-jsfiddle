e4efcad48465f302e78ef5d8f8d0a267cec29abd
cwb <sy_caowubin@aliyun.com>
2019-06-13 12:40:50 +0800

    chore: format tslint.json,在vue.config.js添加别名;

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
 
 module.exports = {
   publicPath: './',
   outputDir: 'docs',
+  lintOnSave: true,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
 }
